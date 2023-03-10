f4f4a028e0333e9936822f021af2d2a3ed35462a
lyukidon <qgx99098@gmail.com>
2023-03-10 16:44:36 +0900

    fix token error

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const getClubMembers = require('./utils/clubMembers');
 const getClubLeagueBattleLog = require('./utils/clubMembersBattleLog')
 
 const getData = async (clubTag, developmentToken) => {
-    const clubMembers = await getClubMembers(clubTag, token)
-    const clubMembersBattleLog = await getClubLeagueBattleLog(clubMembers, token);
+    const clubMembers = await getClubMembers(clubTag, developmentToken)
+    const clubMembersBattleLog = await getClubLeagueBattleLog(clubMembers, developmentToken);
 }
 
 
